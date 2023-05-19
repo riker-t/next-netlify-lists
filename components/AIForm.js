3ba7c47b46b50ed35ad8cded916b715bdce699c6
Teddy Riker <teddyriker2@gmail.com>
2023-05-18 23:14:43 -0400

    adsense

--- components/AIForm.js
+++ components/AIForm.js
@@ -1,5 +1,9 @@
 // ./components/AIForm.js
 import { useState } from 'react';
+import styles from './AIForm.module.css';
+import FullScreenLoading from './FullScreenLoading';
+// import Loading from './Loading';
+import ResponseDisplay from './ResponseDisplay';
 import axios from 'axios';
 
 function AIForm() {
@@ -7,6 +11,7 @@ function AIForm() {
   const [input, setInput] = useState('');
   const [response, setResponse] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState('');
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -23,23 +28,23 @@ function AIForm() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Title:
-          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-        </label>
-        <label>
-          Input:
-          <textarea value={input} onChange={(e) => setInput(e.target.value)} style={{ width: '100%', height: '100px' }} />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-
+    <div className={styles.container}>
       {loading ? (
-        <p>Loading...</p>
+        <FullScreenLoading />
+      ) : response ? (
+        <ResponseDisplay response={response} onTryAgain={() => setResponse(null)} />
       ) : (
-        response && <pre>{JSON.stringify(response, null, 2)}</pre>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <label className={styles.label}>
+            Give your list a name:
+            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className={styles.inputTitle}/>
+          </label>
+          <label className={styles.label}>
+            Enter some content:
+            <textarea value={input} onChange={(e) => setInput(e.target.value)} className={styles.inputContent}/>
+          </label>
+          <button type="submit" className={styles.button}>Submit</button>
+        </form>
       )}
     </div>
   );
