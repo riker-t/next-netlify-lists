15aa20a8b595a5856d96de8f7e264191910b3f3d
Teddy Riker <teddyriker2@gmail.com>
2023-05-21 19:28:43 -0400

    navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '@styles/globals.css'
 import BottomNav from '@components/BottomNav'
+import NavigationBar from '@components/NavigationBar'
 
 function Application({ Component, pageProps }) {
   return (
     <>
+      <NavigationBar/>
       <BottomNav />
       <Component {...pageProps} />
 
