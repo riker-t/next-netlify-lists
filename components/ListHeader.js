dca4ca99e4360f3e5d3cd86fb1f0c316ddd717ff
Teddy Riker <teddyriker2@gmail.com>
2023-05-25 16:43:46 -0400

    example profile

--- components/ListHeader.js
+++ components/ListHeader.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { FaRegSave, FaThumbsUp, FaRegCopy, FaCopy, FaBookmark } from 'react-icons/fa';
 import styles from './ListHeader.module.css';
+import Link from 'next/link';
 
 const ListHeader = ({ userIcon, listName, userName, listLink }) => {
     userIcon = 'https://media.licdn.com/dms/image/C4E03AQH8BQHbbYxnGw/profile-displayphoto-shrink_800_800/0/1563299356210?e=2147483647&v=beta&t=bwzNGxtWDYFGKx2sBPu7DUIht2ww5ZGwbLcJE30XBIk'
     userName = 'Teddy Riker'
     listName = 'Italy Trip'
-    
+
     return (
         <div className={styles.metaContainer}>
             <div className={styles.user}>
-                <img
-                    className={styles.userIcon}
-                    src={userIcon}
-                    alt={userName}
-                    onClick={() => console.log('User icon clicked')}
-                />
+                <Link href="/example_profile">
+                    <img
+                        className={styles.userIcon}
+                        src={userIcon}
+                        alt={userName}
+                        onClick={() => console.log('User icon clicked')}
+                    />
+                </Link>
                 <div className={styles.userInfo}>
                     <p className={styles.listName}>{listName}</p>
                     <p className={styles.userName}>{userName}</p>
---
+++ pages/example_profile.js
@@ -0,0 +1,10 @@
+import styles from './index.module.css'
+import Button from '@components/Button'
+
+export default function ExampleProfile() {
+  return (
+    <div className = {styles.container}>
+        <h1>Sorry, this doesn't exist yet!</h1>
+    </div>
+  )
+}
