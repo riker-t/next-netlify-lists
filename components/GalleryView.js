e09f9bcc75e1f6e225bb9de54b8a2fba70a4a8a7
Teddy Riker <teddyriker2@gmail.com>
2023-05-25 21:07:03 -0400

    swipeable views

--- components/GalleryView.js
+++ components/GalleryView.js
@@ -1,90 +1,60 @@
-import React, { useState } from 'react';
-import { useSwipeable } from 'react-swipeable';
-import { CSSTransition } from 'react-transition-group';
+import React from 'react';
+import SwipeableViews from 'react-swipeable-views-react-18-fix';
+import { FaArrowAltCircleLeft } from 'react-icons/fa';
 import styles from './GalleryView.module.css';
 import itemstyles from './Item.module.css';
 
-import { FaArrowAltCircleLeft } from 'react-icons/fa';
-
-
 function GalleryView({ images, index, onClose }) {
-  const [currentImage, setCurrentImage] = useState(index);
-  const [animating, setAnimating] = useState(false);
-  const [direction, setDirection] = useState('right');
+  const [currentImage, setCurrentImage] = React.useState(index);
 
-  const handlers = useSwipeable({
-    onSwipedLeft: () => {
-      setAnimating(true);
-      setDirection('left');
-      let nextImageIndex = currentImage;
-      do {
-        nextImageIndex = (nextImageIndex + 1) % images.length;
-      } while (images[nextImageIndex].photoUrl === null);
-      setCurrentImage(nextImageIndex);
-    },
-    onSwipedRight: () => {
-      setAnimating(true);
-      setDirection('right');
-      let prevImageIndex = currentImage;
-      do {
-        prevImageIndex = (prevImageIndex - 1 + images.length) % images.length;
-      } while (images[prevImageIndex].photoUrl === null);
-      setCurrentImage(prevImageIndex);
-    },
-    preventDefaultTouchmoveEvent: true,
-    trackMouse: true,
-  });
+  const handleChangeIndex = index => {
+    setCurrentImage(index);
+  };
 
   const indicator = `${currentImage + 1}/${images.length}`;
 
   return (
-    <>
-      <div {...handlers} className={styles.galleryView}>
-        <div className={styles.closeButton} onClick={onClose}>
-          <FaArrowAltCircleLeft />
+    <div className={styles.galleryView}>
+      <div className={styles.closeButton} onClick={onClose}>
+        <FaArrowAltCircleLeft />
+      </div>
 
-        </div>
-
-        <CSSTransition
-          in={animating}
-          timeout={300}
-          classNames={`slide-${direction}`}
-          onEntered={() => setAnimating(false)}
-        >
-          <div className={styles.galleryImageWrapper}>
+      <SwipeableViews index={currentImage} onChangeIndex={handleChangeIndex} resistance>
+        {images.map((image, i) => (
+          <div key={i} className={styles.galleryImageWrapper}>
             <img
-              src={images[currentImage].photoUrl}
-              alt={images[currentImage].name}
+              src={image.photoUrl}
+              alt={image.name}
               className={styles.galleryImage}
             />
           </div>
-        </CSSTransition>
-        <div className={styles.indicator}>{indicator}</div>
+        ))}
+      </SwipeableViews>
+      <div className={styles.indicator}>{indicator}</div>
 
-        <div className={styles.galleryContent}>
-          <div className={itemstyles.itemName}>{images[currentImage].name}</div>
-          {images[currentImage].description && (
-            <div className={itemstyles.itemDescription}>
-              {images[currentImage].description}
-            </div>
-          )}
-          <div >
-            {images[currentImage].ctas &&
-              images[currentImage].ctas.map((cta) => (
-                <a
-                  key={cta.title}
-                  href={cta.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={itemstyles.itemCta}
-                >
-                  {cta.title}
-                </a>
-              ))}
+      <div className={styles.galleryContent}>
+        <div className={itemstyles.itemName}>{images[currentImage].name}</div>
+        {images[currentImage].description && (
+          <div className={itemstyles.itemDescription}>
+            {images[currentImage].description}
           </div>
+        )}
+        <div >
+          {images[currentImage].ctas &&
+            images[currentImage].ctas.map((cta) => (
+              <a
+                key={cta.title}
+                href={cta.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={itemstyles.itemCta}
+              >
+                {cta.title}
+              </a>
+            ))}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
