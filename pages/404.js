0a1543054e10d07bd7966591901bf6a92c79da6d
Teddy Riker <teddyriker2@gmail.com>
2023-05-25 23:54:54 -0400

    routig

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import AIForm from '@components/AIForm'
+import AIFormAsync from '@components/AIFormAsync'
+
+export default function DeadPage() {
+    return (
+        <div className={{padding: 20}}>
+           <h1>Sorry, this doesn't exist yet!</h1>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,8 @@ export default function Home() {
         <h1>Welcome to Lists!</h1>
         <div className = {styles.buttonsContainer}>
           <Button link='/example_list_new' cta = 'See an Example'/>
-          <Button link='/example_profile' cta = 'Search for Lists or People'/>
-          <Button link='/example_profile' cta = 'Create a List'/>
+          <Button link='/404' cta = 'Search for Lists or People'/>
+          <Button link='/404' cta = 'Create a List'/>
         </div>
         {/* <Header title="Welcome to Lists!" /> */}
         {/* <p className="description">
